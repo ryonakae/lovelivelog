d6e404b19275b783909c9c6fe53cb79b897d4898
Ryo Nakae <me@ryonakae.com>
2014-03-18 00:27:40 +0900

    引き続きPC表示進んだ

--- source/javascripts/lovelivelog.js
+++ source/javascripts/lovelivelog.js
@@ -7,23 +7,64 @@ $(function(){
   });
 */
 
-  // #main window resize
+
+  // common variables
   var mainHeight;
+  var headerHeight;
+
+
+  // #main window resize
   $(window).on("load resize", function(){
-    mainHeight = $(window).height() - $("#header").outerHeight();
+    headerHeight = $("#header").outerHeight()
+    mainHeight = $(window).height() - headerHeight;
+
     $("#main").css({
       "width" : $(window).width(),
       "height" : mainHeight
     });
   });
 
+
+  // header fixed
+  var mainVisualHeight;
+  var scroll;
+
+  $(window).scroll(function(){
+    scroll = $(this).scrollTop();
+
+    if ( scroll > mainHeight ) {
+      $("#header").addClass("fixed");
+      $("#header").removeAttr("style");
+    }
+    else {
+      $("#header").removeClass("fixed");
+      $("#header").css({ "top" : mainHeight });
+    }
+  });
+
+
   // gobottom
   $("#gobottom").on("click", function(){
-    $("html, body").stop().animate({ scrollTop : mainHeight }, 500, "easeInOutCubic");
+    $("html, body").stop().animate({ scrollTop : mainHeight }, 1000, "easeInOutCubic");
   });
+
 
   // backtop
   $("#backtop").on("click", function(){
-    $("html, body").stop().animate({ scrollTop : 0 }, 1500, "easeOutCubic");
+    $("html, body").stop().animate({ scrollTop : 0 }, 1500, "easeInOutCubic");
+  });
+
+
+  // smooth scroll
+  $("a[href^=#]").on("click", function() {
+    var speed = 600;
+    var easing = "easeInOutCubic";
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var offset = target.offset().top;
+
+    $("html, body").animate({ scrollTop : offset - headerHeight - 20 }, speed, easing);
+
+    return false;
   });
 });
