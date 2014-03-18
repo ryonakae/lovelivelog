fe5abf652f05d22ac136de7a771425557d21c333
Ryo Nakae <me@ryonakae.com>
2014-03-19 03:37:14 +0900

    ちょい修正
    フォントのURLは依然直せず

--- config.rb
+++ config.rb
@@ -48,7 +48,7 @@ activate :livereload
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
-set :fonts_dir, 'fonts'
+# set :fonts_dir, 'stylesheets/fonts'
 
 # Build-specific configuration
 configure :build do
